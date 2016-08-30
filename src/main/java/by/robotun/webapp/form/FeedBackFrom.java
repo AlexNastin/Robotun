f57b8fc1f036944a4c2400a4cc9111047fcc32d2
Nastin <Nastin@AP-AST12.irc.beltelecom.by>
2016-08-30 13:17:51 +0300

    FeedBack

--- src/main/java/by/robotun/webapp/form/FeedBackFrom.java
+++ src/main/java/by/robotun/webapp/form/FeedBackFrom.java
@@ -10,28 +10,43 @@ public class FeedBackFrom implements Essence {
 	private String name;
 	private String title;
 	private String text;
-	
+
+	public FeedBackFrom() {
+		this.email = "";
+		this.name = "";
+		this.title = "";
+		this.text = "";
+
+	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getText() {
 		return text;
 	}
+
 	public void setText(String text) {
 		this.text = text;
 	}
